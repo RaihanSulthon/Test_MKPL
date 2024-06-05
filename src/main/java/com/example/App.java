d8eea659dfee9a5c80f5908b71c7925df141cb96
RaihanSulthon <raihansy@student.telkomuniversity.ac.id>
2024-06-05 09:10:39 +0700

    update patch

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -34,7 +34,7 @@ public class App {
                 if (num2 != 0) {
                     return num1 / num2;
                 } else {
-                    throw new ArithmeticException("Error! Pembagian dengan nol tidak diperbolehkan.");
+                    throw new ArithmeticException("Error! Pembagian dengan nol tidak bisa.");
                 }
             }
             default -> throw new IllegalArgumentException("Operator tidak valid!");
